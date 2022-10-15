dcf72508212ceb8eca1e077989eb9d24fbdcbfd1
iolymmoliveira <iolymmoliveira@gmail.com>
2022-10-15 17:03:52 -0300

    Edição JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,79 @@
-const calcularIMC = document.getElementsByClassName('calcular');
+/* Variável de escopo global referente ao botão calcular */
+const calcular = document.getElementById('calcular');
 
 function calcIMC() {
+  /*Pegar os dados nos inputs e armazenar em uma variável*/
+  const select = document.getElementById('sexo');
+  const peso = document.getElementById('peso').value;
+  const altura = document.getElementById('altura').value;
+  const sexo = select.options[select.selectedIndex].value;
+  const resultadoNum = document.getElementById('resultadoNum');
+  const resultadoTexto = document.getElementById('resultadoTexto');
 
-  /*Pega os dados nos inputs e armazena em uma variável*/
-  const peso = document.getElementsByClassName('peso').value;
-  const altura = document.getElementsByClassName('altura').value;
-  const sexo = document.getElementsByClassName('sexo').value;
-  const resultadoNum = document.getElementsByClassName('resultadoNum');
-  const resultadoTexto = document.getElementsByClassName('resultadoTexto');
+  calculaIMCeValidaInputs();
+}
 
-  /*No caso dos campos estarem preenchidos o calculo é feito*/
-  if(peso !== '' && altura !== '') {
-    
-    const valorIMC = ( peso / (altura * altura)).toFixed(2);
+/* Função para validar se os campos estão preenchidos*/
+function calculaIMCeValidaInputs() {
+  if (peso.value !== '' && altura.value !== '') {
+  
+    /* Realiza o cálculo do imc e armazena na variável */
+    const resultadoIMC = (peso.value / (altura.value * altura.value)).toFixed(2);
 
-    let img = document.getElementsByClassName('image');
+    let img = document.getElementById('image');
     let texto = '';
 
-    if (valorIMC >= 30) {
-      img.src = './imgs/01.png';
+    if (resultadoIMC >= 30) {
+      if (sexo.value == 'Feminino') {
+        img.src = 'assets/imgs/lisa-simpson.png';
+      } else {
+        img.src = 'assets/imgs/clancy-simpson.png';
+      }
       texto = 'Peso Pesadoo!';
-      document.body.style.backgroundColor = '#red';
-    } 
-    else if (valorIMC >= 25) {
-      img.src = './imgs/homer-simpson.png';
+      document.querySelector('.container-right').style.backgroundColor = 'rgba(245, 35, 35, 0.986)';
+    }
+    else if (resultadoIMC >= 25) {
+      if (sexo.value == 'Feminino') {
+        img.src = 'assets/imgs/cletus-spuckler.png';
+      } else {
+        img.src = 'assets/imgs/homer-simpson.png';
+      }
       texto = 'Táa gordinhooo(aaa)!';
-      document.body.style.backgroundColor = '#yellow';
-    } 
-    else if (valorIMC >= 19) {
-      img.src = './imgs/popeye.png';
+      document.querySelector('.container-right').style.backgroundColor = 'rgb(255, 255, 0)';
+    }
+    else if (resultadoIMC >= 19) {
+      if (sexo.value == 'Feminino') {
+        img.src = 'assets/imgs/marge-simpson.png';
+      } else {
+        img.src = 'assets/imgs/waylon-smithers.png';
+      }
       texto = 'Normalzãoo!';
-      document.body.style.backgroundColor = '#green';
-    } 
+      document.querySelector('.container-right').style.backgroundColor = 'rgb(81, 243, 81)';
+    }
     else {
-      img.src = './imgs/magrela.jpg';
+      if (sexo.value == 'Feminino') {
+        img.src = 'assets/imgs/olivia-palito.png';
+      } else {
+        img.src = 'assets/imgs/shaggy-rogers.png';
+      }
       texto = 'Chassi de Grilo, muito baixo!';
-      document.body.style.backgroundColor = '#orange';
+      document.querySelector('.container-right').style.backgroundColor = 'rgb(245, 158, 100)';
     }
 
-    resultadoNum.textContent = `${valorIMC}`;
+    resultadoNum.textContent = `${resultadoIMC}`;
     resultadoTexto.textContent = `${texto}`;
   }
   else {
-    resultadoNum.textContent = 'Todos os campos devem ser preenchidos!';
+  resultadoNum.textContent = 'Todos os campos devem ser preenchidos!';
   }
 }
 
-calcularIMC.addEventListener('click', calcIMC);
+/* O botão calcular recebe a propriedade de escutar o evento de click e ao ocorrer executa a função passada */
+calcular.addEventListener('click', calcIMC);
 
 document.addEventListener('keypress', function(e) {
   if(e.key === 'Enter') {
-    const botao = document.querySelector('.calcular');
+    const botao = document.querySelector('#calcular');
     botao.click();
   }
 })
